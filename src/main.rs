b007f10008fc88038c4ac1dc3f0a4785db52f79f
Plurmp <plurmpmcflurnt@gmail.com>
2023-01-07 22:56:48 -0600

    little bit of progress on structure

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,90 @@
-fn main() {
-    println!("Hello, world!");
+mod commands;
+
+use std::collections::HashSet;
+use std::sync::Arc;
+
+use serenity::gateway::Shard;
+use serenity::{async_trait, framework};
+use serenity::client::bridge::gateway::ShardManager;
+use serenity::framework::standard::macros::group;
+use serenity::framework::StandardFramework;
+use serenity::http::Http;
+use serenity::model::event::ResumedEvent;
+use serenity::model::gateway::Ready;
+use serenity::prelude::*;
+use tracing::{error, info};
+
+pub struct ShardManagerContainer;
+
+impl TypeMapKey for ShardManagerContainer {
+    type Value = Arc<Mutex<ShardManager>>;
+}
+
+struct Handler;
+
+#[async_trait]
+impl EventHandler for Handler {
+    async fn ready(&self, _: Context, ready: Ready) {
+        info!("{} is connected", ready.user.name);
+    }
+    async fn resume(&self, _: Context, _: ResumedEvent) {
+        info!("Resumed");
+    }
+}
+
+#[group]
+#[commands()]
+struct General;
+
+#[group]
+#[commands()]
+struct Webhook;
+
+
+#[tokio::main]
+async fn main() {
+    dotenv::dotenv().expect("failed to load .env file");
+
+    let token = std::env::var("DISCORD_TOKEN").expect("Expected token in env");
+    let http = Http::new(&token);
+
+    let (owners, _bot_id) = match http.get_current_application_info().await {
+        Ok(info) => {
+            let mut owners = HashSet::new();
+            owners.insert(info.owner.id);
+
+            (owners, info.id)
+        },
+        Err(why) => panic!("Could not access app info: {:?}", why)
+    };
+
+    let framework = StandardFramework::new()
+        .configure(|c| c.owners(owners).prefix(">"))
+        .group(&GENERAL_GROUP);
+
+    let intents = GatewayIntents::GUILD_MESSAGES 
+        | GatewayIntents::DIRECT_MESSAGES 
+        | GatewayIntents::MESSAGE_CONTENT;
+
+    let mut client = Client::builder(&token, intents)
+        .framework(framework)
+        .event_handler(Handler)
+        .await
+        .expect("Error creating client");
+
+    {
+        let mut data = client.data.write().await;
+        data.insert::<ShardManagerContainer>(client.shard_manager.clone());
+    }
+
+    let shard_manager = client.shard_manager.clone();
+
+    tokio::spawn(async move {
+        tokio::signal::ctrl_c().await.expect("Could not register ctrl+c handler");
+        shard_manager.lock().await.shutdown_all().await;
+    });
+
+    if let Err(why) = client.start().await {
+        println!("error: {:?}", why);
+    }
 }
